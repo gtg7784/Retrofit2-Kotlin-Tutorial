4e5c8ae13e907dac17d9380286fd4a91ca236415
gtg7784 <tae.gun7784@gmail.com>
2021-01-11 18:44:11 +0900

    Add Weather Service

--- app/src/main/java/co/gtg7784/retrofitkotlinstudy/WeatherService.kt
+++ app/src/main/java/co/gtg7784/retrofitkotlinstudy/WeatherService.kt
@@ -1,4 +1,13 @@
 package co.gtg7784.retrofitkotlinstudy
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface WeatherService {
+    @GET("data/2.5/weather")
+    fun getWeatherData (
+        @Query("q") city: String,
+        @Query("appid") appId: String
+    ) : Call<WeatherResponse>
 }
